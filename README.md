e19f1b8a0bac6c2d8434b42dd38f5326d0c204d9
Florian Bruggisser <cansik@users.noreply.github.com>
2021-02-11 19:04:57 +0100

    implemented first basic version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # pymesh2depthmap
-A simple tool to create depth-maps out of 3d meshes.
+A simple tool to create depth-maps out of 3d meshes by using raytracing.
+
+![mesh](output/color.png "Mesh") ![depth](output/depth.png "Depth")
+
+### Installation
+
+#### Conda
+Conda is recommended to install pyembree (speeds up rendering).
+
+```
+conda create -n venv_pym2d python=3.7
+conda activate venv_pym2d
+
+# for linux
+conda config --add channels conda-forge
+conda install -c conda-forge pyembree
+
+# manuall install for windows
+git clone https://github.com/scopatz/pyembree.git
+cd pyembree
+conda install cython numpy
+conda install -c conda-forge embree
+set INCLUDE=%CONDA_PREFIX%\Library\include
+set LIB=%CONDA_PREFIX%\Library\lib
+python setup.py install --prefix=%CONDA_PREFIX%
+```
+
+And install the necessary pip packages as described following.
+
+#### PIP
+
+It's also possible to just install the pip packages.
+
+```
+pip install -r requirements.txt
+```
---
+++ pymesh2depthmap.py
@@ -0,0 +1,68 @@
+"""
+raytrace.py
+----------------
+A very simple example of using scene cameras to generate
+rays for image reasons.
+Install `pyembree` for a speedup (600k+ rays per second)
+"""
+from __future__ import division
+
+import PIL.Image
+
+import trimesh
+import numpy as np
+
+if __name__ == '__main__':
+    print("setting up scene...")
+
+    # test on a simple mesh (enable embree if installed)
+    mesh = trimesh.load('models/bun_zipper.ply')
+
+    # scene will have automatically generated camera and lights
+    scene = mesh.scene()
+
+    # any of the automatically generated values can be overridden
+    # set resolution, in pixels
+    scene.camera.resolution = [256, 256]
+
+    # set field of view, in degrees
+    # make it relative to resolution so pixels per degree is same
+    scene.camera.fov = 50 * (scene.camera.resolution /
+                             scene.camera.resolution.max())
+
+    # convert the camera to rays with one ray per pixel
+    origins, vectors, pixels = scene.camera_rays()
+
+    print("raytracing...")
+    # do the actual ray- mesh queries
+    points, index_ray, index_tri = mesh.ray.intersects_location(
+        origins, vectors, multiple_hits=False)
+
+    # for each hit, find the distance along its vector
+    depth = trimesh.util.diagonal_dot(points - origins[0],
+                                      vectors[index_ray])
+    # find pixel locations of actual hits
+    pixel_ray = pixels[index_ray]
+
+    # create a numpy array we can turn into an image
+    # doing it with uint8 creates an `L` mode greyscale image
+    a = np.zeros(scene.camera.resolution, dtype=np.uint8)
+
+    # scale depth against range (0.0 - 1.0)
+    depth_float = ((depth - depth.min()) / depth.ptp())
+
+    # convert depth into 0 - 255 uint8
+    depth_int = (depth_float * 255).round().astype(np.uint8)
+
+    # assign depth to correct pixel locations
+    a[pixel_ray[:, 0], pixel_ray[:, 1]] = depth_int
+
+    # create a PIL image from the depth queries
+    img = PIL.Image.fromarray(a).rotate(90)
+
+    # show the resulting image
+    img.save("output/depth.png")
+
+    # create a raster render of the same scene using OpenGL
+    rendered = PIL.Image.open(trimesh.util.wrap_as_stream(scene.save_image()))
+    rendered.save("output/color.png")
